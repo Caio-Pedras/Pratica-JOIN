d73af104ba1d5ba3f92d293a11b6c2121994a038
CaioPedras <caio.vp@gmail.com>
2022-07-26 19:55:31 -0300

    FIX Q3

--- Pratica.sql
+++ Pratica.sql
@@ -17,6 +17,7 @@ JOIN users ON educations."userId" = users.id
 JOIN courses ON educations."courseId" = courses.id
 JOIN schools ON educations."schoolId" = schools.id
 WHERE users.id = 30
+AND educations.status = 'finished'
 
 -- Questão 4:
 SELECT users.id, users.name, roles.name AS role, companies.name AS company, experiences."startDate" 
